4139a84e102ca557353a878054bad28c757f71c1
Denis Cheung <d.s.h.cheung@gmail.com>
2014-11-28 15:27:36 +0800

    edited calculate prices button, added blur for inputs to enable instant total price, replace broswer alert to html alert, fixed green/gray/red input problem.

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,24 +1,35 @@
-$("button#calc-prices-button").click(
-  function() {
-    shoppingCart.makeCart();
-    if (shoppingCart.alertUserInputError() == false) {
-      $('#total-price').html("INVALID QUANTITY");
-      $('#total-price').parent().addClass("has-error");
-    } else {
-      $('#total-price').html(shoppingCart.calcTotalPrice());
-      $('#total-price').parent().removeClass("has-error");
-    }
+$(document).ready(
+  function(){
+    $("button#calc-prices-button").click(
+      function() {
+        $('#total-price').html("$" + (shoppingCart.calcTotalPrice()).toFixed(2));
+      }
+    );
+    $('.quantity').blur(
+      function (){
+        for (var index = 0; index < $('.quantity').length; index++){
+          if (this == $('.quantity')[index]){
+            shoppingCart.judgeInputField(index);
+          }
+        }
+        $('#total-price').html("$" + (shoppingCart.calcTotalPrice()).toFixed(2));
+      }
+    );
+    $('button#delete-entry').click(
+
+    );
   }
 );
 
 var shoppingCart = {
   cart: [],
   calcTotalPrice: function() {
+    this.makeCart();
     var totalPrice = 0;
     for (index in this.cart) {
       totalPrice += this.calcItemPrice(index);
     }
-    return "$" + this.centToDollar(totalPrice);
+    return this.centToDollar(totalPrice);
   },
   calcItemPrice: function(index) {
     return this.cart[index].price * this.cart[index].quantity;
@@ -34,6 +45,7 @@ var shoppingCart = {
       };
       this.cart.push(itemObj);
     }
+    this.alertUserInputError();
     return this.cart;
   },
   getCleanItemName: function(index) {
@@ -50,19 +62,40 @@ var shoppingCart = {
   },
   getCleanItemQuantity: function(index) {
     var quantityNum = $('.quantity')[index].value;
-    if (quantityNum && quantityNum >= 0) {
-      this.makeFieldGreen(index, false);
+    if (quantityNum > 0) {
       this.inputError.push(true);
       return quantityNum;
-    } else {
-      this.makeFieldRed(index, true);
+    } 
+    else if (quantityNum == 0){
+      return 0;
+    }
+    else {
       this.inputError.push(false);
+      return 0;
+    }
+  },
+  judgeInputField: function(index) {
+    var quantityNum = $('.quantity')[index].value;
+    if (quantityNum > 0) {
+      this.makeFieldGreen(index);
+      return;
+    }
+    else if (quantityNum == 0) {
+      this.makeFieldNothing(index)
+      return;
+    }
+    else {
+      this.makeFieldRed(index);
+      return;
     }
   },
   makeFieldGreen: function(index) {
     $($('.quantity')[index]).parent().addClass('has-success');
     $($('.quantity')[index]).parent().removeClass('has-error');
-
+  },
+  makeFieldNothing: function(index){
+    $($('.quantity')[index]).parent().removeClass('has-success');
+    $($('.quantity')[index]).parent().removeClass('has-error');
   },
   makeFieldRed: function(index) {
     $($('.quantity')[index]).parent().removeClass('has-success');
@@ -70,20 +103,21 @@ var shoppingCart = {
   },
   inputError: [],
   alertUserInputError: function() {
-    for (index in this.inputError) {
-      if (this.inputError[index] == false) {
-        alert(
-          "Cannot calculate price!\n" +
-          "Invalid input is highlighted in red!\n" +
-          "Please make sure your quantity is a number!"
-        );
-        return false;
-      } else {
-        return true;
+    var hasNoError = true;
+    for (index in this.inputError){
+      if (this.inputError[index] == false){
+        hasNoError = false;
       }
+    }
+    if (hasNoError) {
+        $('#alert-message').addClass('hidden');
+    } else {
+        $('#alert-message').removeClass('hidden');
+        $('#alert-message').text(this.errorMessageAlert);
     }
   },
   centToDollar: function(cent) {
     return cent / 100;
-  }
+  },
+  errorMessageAlert: ['We have ignore some of your values because they were invalid!']
 }
